6ef615db4ecf85433903879dae399ead64783e08
Maven <v@maweis.com>
2012-05-09 11:09:59 +0800

    add restful client code

--- web/uups/restful_client.py
+++ web/uups/restful_client.py
@@ -1,6 +1,6 @@
 import urllib2
 opener = urllib2.build_opener(urllib2.HTTPHandler)
-request = urllib2.Request('http://localhost:8888/test/1234', data='put_data')
+request = urllib2.Request('http://192.168.91.171:8000/user/1234', data='put_data')
 #request = urllib2.Request('http://localhost:8888/test/')
 request.add_header('Content-Type', 'your/contenttype')
 request.get_method = lambda: 'GET'
